922dd46a796b787bcc7e05553d1a488d6a710939
Dalton <n7dalton@gmail.com>
2023-04-20 09:53:25 -0500

    Initial commit

---
+++ Assets/Scripts/MoneyScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyScript : MonoBehaviour
+{
+    public TextAlignment text;
+    public int MoneyCounter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
